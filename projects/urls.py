241c873c433f319948024ab17cf368e8119f638c
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-07-13 14:27:40 +0000

    Install django rest framework

---
+++ projects/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from projects import views
+
+urlpatterns = [
+    path('projects/', views.ProjectList.as_view()),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Project
 
-def say_hello(request):
-    return HttpResponse('hello')
+
+class ProjectList(APIView):
+    def get(self, request):
+        projects = Project.objects.all()
+        return Response(projects)
