b6e5442f5e0c42d24c3e0eae4f2100f830dec28f
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-07-13 14:17:11 +0000

    feat: Create Project model structure

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(
+        upload_to='images/', default='../profile-pic_nfhakf'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s project"
