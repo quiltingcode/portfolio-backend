e229f13154d47fb5e566be0fda75731e5be5c1d5
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-07-14 10:06:39 +0000

    feat: Add fields to project model

--- projects/models.py
+++ projects/models.py
@@ -6,12 +6,15 @@ class Project(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255, default='project')
+    description = models.TextField(blank=True, default='a brilliant project')
+    tech_stack = models.CharField(max_length=255, blank=False, default='HTML')
     image = models.ImageField(
         upload_to='images/', default='../profile-pic_nfhakf'
     )
-
+    
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return f"{self.owner}'s project"
+        return f"{self.owner}'s {self.title}"
--- projects/serializers.py
+++ projects/serializers.py
@@ -5,9 +5,24 @@ from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size is larger than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        return value
+
     class Meta:
         model = Project
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'image',
-
+            'id', 'owner', 'created_at', 'updated_at', 'title', 'description',
+            'image', 'tech_stack',
         ]
