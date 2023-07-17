ce9349a60050ea9ded8d95feca0787df65176815
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-07-17 11:44:29 +0000

    feat: Format date time for comments

--- comments/serializers.py
+++ comments/serializers.py
@@ -4,6 +4,17 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Comment model
+    """
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta:
         model = Comment
